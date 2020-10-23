d318544a310e5fa9f82b5f2c0d3a1a35a41eacc6
Ashgan <ashganwiki@gmail.com>
2020-10-23 12:33:14 +0200

    first commit

---
+++ task-2.js
@@ -0,0 +1,10 @@
+
+let key  = "h"
+
+let sentence = "The relationship between the global city and its hinterland is one that usually glorifies the former as a dynamo of change, dragging the rest of the country along with it economically, socially and culturally. But in poor and ethnically diverse countries such as Sudan, these influences need to go the other way, to seep through to the margins of the state in order to create a strong national identity and develop the country all at once. In such weak and ethnically fractured states, cities can be parasites, feeding on their states, needing them to stay alive to thrive. The irony is that, as with all parasites, they inevitably end up killing themselves by exhausting their hosts."
+ 
+
+console.log(` ${matchs.length} match(s) was found`)
+
+ 
+
